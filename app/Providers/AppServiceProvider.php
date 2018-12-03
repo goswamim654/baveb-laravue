3884fed70559bacb297d8994993957c3d0d45a03
Jay J Das <jay3000bc@gmail.com>
2018-12-03 12:17:13 +0530

    env changes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,9 +15,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-        if (env('APP_ENV') === 'production' || env('APP_ENV') === 'dev' || env('APP_ENV') === 'local') {
-             \URL::forceScheme('https');
-        }
     }
 
     /**
